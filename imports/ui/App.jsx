165781394e86429e83310481621b767e7b31eb71
Arpita14378 <arpitahk@gmail.com>
2021-04-11 13:47:21 +0530

    cleanup level 1

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,6 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
 import HomePage from './HomePage'
-import NavBar from './NavBar'
 import NewNavbar from './NewNavbar'
 import Login from './components/Login'
 export const  App=() =>{
